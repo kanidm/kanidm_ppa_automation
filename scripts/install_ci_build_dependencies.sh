4949d361616b69b55657ca4231fc219c00a7e71d
Jinna Kiisuo <jinnak@nocturnal.fi>
2024-10-05 01:39:26 +0300

    fix: Modify ubuntu multiarch patching for GHA changes

--- scripts/install_ci_build_dependencies.sh
+++ scripts/install_ci_build_dependencies.sh
@@ -25,7 +25,8 @@ DEB_ARCH="${2?}" # Expecting "amd64" or "arm64" etc
 if [[ "$DEB_OS" == "ubuntu" ]]; then
     2>&1 echo "Patching Ubuntu apt sources for multiarch"
     # Default entries do not pin an arch, fix that
-    sed -E 's/^deb (http|mirror)/deb [arch=amd64] \1/' -i '/etc/apt/sources.list'
+    # GHA since 24.04 keeps them in a separate file
+    sed -E 's/^deb (http|mirror)/deb [arch=amd64] \1/' -i /etc/apt/sources.list /etc/apt/sources.list.d/ubuntu.sources
     # arm64 is on a completely different mirror structure, add that pinned to arm64
     echo 'deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports/ jammy main restricted' > /etc/apt/sources.list.d/arm64.list
     echo 'deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports/ jammy-updates main restricted' >> /etc/apt/sources.list.d/arm64.list
@@ -34,7 +35,7 @@ fi
 # From here on normal Debian multiarch logic applies
 2>&1 echo "Enabling multiarch"
 dpkg --add-architecture $DEB_ARCH
-apt-get update || cat /etc/apt/sources.list
+apt-get update || cat /etc/apt/sources.list.d/ubuntu.sources
 apt-get install -y \
     libssl3:$DEB_ARCH \
     libpam0g:$DEB_ARCH \
