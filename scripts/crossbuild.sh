ea933c3fda025ae024970e656fce4aaee75683b5
Jinna Kiisuo <jinnak@nocturnal.fi>
2024-10-04 18:02:54 +0300

    refactor: Crossbuild simplification, multitarget runs

--- scripts/crossbuild.sh
+++ scripts/crossbuild.sh
@@ -2,43 +2,52 @@
 
 set -e
 
+BASEDIR="$(dirname $0)/.."
+
 if [ -z "$1" ]; then
     echo "Usage: $0 <target>"
-    if [ -d ./crossbuild ]; then
+    if [ -d "${BASEDIR}/crossbuild" ]; then
         echo "Valid targets:"
-        find crossbuild/ -maxdepth 1 -mindepth 1 -type d | awk -F'/' '{print $NF}' | sort
+        find "${BASEDIR}/crossbuild/" -maxdepth 1 -mindepth 1 -type d | awk -F'/' '{print $NF}' | sort
     else
-        echo "This script must be run from the repo root (crossbuild dir missing)."
+	echo "Missing crossbuild configs, cannot proceed."
     fi
     exit 1
 fi
 
-if [ ! -d "crossbuild/$1" ]; then
-    echo "Could not find target at: crossbuild/$1"
-    exit 1
-fi
+# Iterate over given targets.
+targets=("$@")
+for target in "${targets[@]}"; do
+	if [ ! -d "${BASEDIR}/crossbuild/$1" ]; then
+	    echo "Could not find target at: ${BASEDIR}/crossbuild/$1"
+	    exit 1
+	fi
 
-# Find the target rust architecture
-TRIPLET=$(echo $1 | cut -d \- -f 3-)
-echo "Crossbuilding for: $TRIPLET"
-rustup target add "$TRIPLET"
+	# Find the target rust architecture
+	OS=$(echo $1 | cut -d \- -f 1-2)
+	TRIPLET=$(echo $1 | cut -d \- -f 3-)
+	echo "Crossbuilding for: $TRIPLET"
+	rustup target add "$TRIPLET"
 
-CROSS_CONFIG="crossbuild/${1}/Cross.toml" \
-    cross build --target "$TRIPLET" \
-        --bin kanidm_unixd \
-        --bin kanidm_unixd_tasks \
-        --bin kanidm_ssh_authorizedkeys \
-        --bin kanidm-unix \
-        --release
-CROSS_CONFIG="crossbuild/${1}/Cross.toml" \
-    cross build --target "$TRIPLET" \
-        -p pam_kanidm \
-        -p nss_kanidm \
-        --release
+	export CROSS_CONFIG="${BASEDIR}/crossbuild/${OS}.toml"
 
-TRIPLET=$(echo $1 | cut -d \- -f 3-)
+	cross build --target "$TRIPLET" \
+		--bin kanidm_unixd \
+		--bin kanidm_unixd_tasks \
+		--bin kanidm_ssh_authorizedkeys \
+		--bin kanidm-unix \
+		--release
+	cross build --target "$TRIPLET" \
+		-p pam_kanidm \
+		-p nss_kanidm \
+		--release
 
-echo "Build artefacts for ${TRIPLET}:"
-find "./target/${TRIPLET}/release/" -maxdepth 1 \
-    -type f -not -name '*.d' \
+	echo "Build artefacts for ${TRIPLET}:"
+	find "./target/${TRIPLET}/release/" -maxdepth 1 \
+    	    -type f -not -name '*.d' -not -name '*.rlib' \
+	    -name '*kanidm*'
+done
+echo "All current artefacts across targets:"
+find ./target/*/release/ -maxdepth 1 \
+    -type f -not -name '*.d' -not -name '*.rlib' \
     -name '*kanidm*'
