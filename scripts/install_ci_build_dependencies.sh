fe20694997cfb6e66c691028d4c925dade00afa8
Jinna Kiisuo <jinnak@nocturnal.fi>
2024-10-04 18:12:26 +0300

    refactor: Change ci_build_deps to require root instead of using sudo

--- scripts/install_ci_build_dependencies.sh
+++ scripts/install_ci_build_dependencies.sh
@@ -11,6 +11,11 @@ fi
 
 set -eu
 
+if [[ "$UID" != 0 ]]; then
+    >&2 echo "Error, this script must be run as root."
+    exit 1
+fi
+
 DEB_OS="${1?}" # Expecting "debian" or "ubuntu"
 DEB_ARCH="${2?}" # Expecting "amd64" or "arm64" etc
 
@@ -20,17 +25,17 @@ DEB_ARCH="${2?}" # Expecting "amd64" or "arm64" etc
 if [[ "$DEB_OS" == "ubuntu" ]]; then
     2>&1 echo "Patching Ubuntu apt sources for multiarch"
     # Default entries do not pin an arch, fix that
-    sudo sed -E 's/^deb (http|mirror)/deb [arch=amd64] \1/' -i '/etc/apt/sources.list'
+    sed -E 's/^deb (http|mirror)/deb [arch=amd64] \1/' -i '/etc/apt/sources.list'
     # arm64 is on a completely different mirror structure, add that pinned to arm64
-    echo 'deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports/ jammy main restricted' | sudo tee /etc/apt/sources.list.d/arm64.list
-    echo 'deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports/ jammy-updates main restricted' | sudo tee -a /etc/apt/sources.list.d/arm64.list
+    echo 'deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports/ jammy main restricted' > /etc/apt/sources.list.d/arm64.list
+    echo 'deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports/ jammy-updates main restricted' >> /etc/apt/sources.list.d/arm64.list
 fi
 
 # From here on normal Debian multiarch logic applies
 2>&1 echo "Enabling multiarch"
-sudo dpkg --add-architecture $DEB_ARCH
-sudo apt-get update
-sudo apt-get install -y \
+dpkg --add-architecture $DEB_ARCH
+apt-get update
+apt-get install -y \
     libssl3:$DEB_ARCH \
     libpam0g:$DEB_ARCH \
     libudev1:$DEB_ARCH
