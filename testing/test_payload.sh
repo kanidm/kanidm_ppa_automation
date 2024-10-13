bc59d05649fb5b87fd47ca35bda2a81999d48759
Jinna Kiisuo <jinnak@nocturnal.fi>
2024-10-14 00:46:41 +0300

    refactor: Switch testing to base from a repo snapshot instead of debs

--- testing/test_payload.sh
+++ testing/test_payload.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
-ls *.deb
+
+IDM_URI="${1?}"
+IDM_GROUP="${2?}"
+MIRROR_PORT="${3?}"
+
+set -eu
+
+function debug(){
+	>&2 echo "Something went wrong, pausing for debug, to connect:"
+	>&2 echo "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@localhost -p 2222 -i ssh_ed25519"
+	sleep infinity
+}
 
 # Make apt shut up about various things to see relevant output better
 export DEBIAN_FRONTEND=noninteractive
 export LC_CTYPE=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 
-# Speed up aarch64 images that have snapd because it keeps churning back and forth with apt calls
->&2 echo "Disabling snapd, if it's there"
-systemctl disable --now snapd.socket
-systemctl disable --now snapd.seeded.service
-systemctl disable --now snapd.service
+source /etc/os-release
+sed "s/%MIRROR_PORT%/${MIRROR_PORT}/;s/%VERSION_CODENAME%/${VERSION_CODENAME}/" kanidm_ppa.list > /etc/apt/sources.list.d/kanidm_ppa.list
+mv kanidm_ppa.asc /etc/apt/trusted.gpg.d/
 
-apt update
-apt install -y zsh  # So if the test user has zsh, it'll work here
-
-# The alphabetical order just happens to be the right order, but the libs need to be intalled first.
-for pkg in *.deb; do
-	apt install -y "./${pkg}"
-done
+apt update || debug
+apt install -y zsh kanidm-unixd kanidm || debug
 
 >&2 echo "Configuring kanidm-unixd"
+sed "s_#uri.*_uri = \"${IDM_URI}\"_" -i /etc/kanidm/config
+sed "s@#pam_allowed_login_groups.*@pam_allowed_login_groups = \[\"${IDM_GROUP}\"\]@" -i /etc/kanidm/unixd
 
-mkdir /etc/kanidm
-mv unixd.toml /etc/kanidm/unixd
-mv kanidm.toml /etc/kanidm/config
-
->&2 echo "Starting unixd"
-
-systemctl start kanidm-unixd.service
-systemctl start kanidm-unixd-tasks.service
+>&2 echo "Restarting unixd"
+systemctl restart kanidm-unixd.service || debug
 
 >&2 echo "Configuring NSS"
 sed -E 's/(passwd|group): (.*)/\1: \2 kanidm/' -i /etc/nsswitch.conf
@@ -43,7 +43,7 @@ AuthorizedKeysCommand /usr/sbin/kanidm_ssh_authorizedkeys %u
 AuthorizedKeysCommandUser nobody
 LogLevel DEBUG1
 EOT
-systemctl restart ssh.service
+systemctl restart ssh.service || debug
 
 >&2 echo "Go test ssh login! Do a ^C here when you're done"
 >&2 echo "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null localhost -p 2222"
