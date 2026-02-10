e3d7903fd5921fd644f3d806b44b53e1479348e6
Jinna Kiisuo <jinnak@nocturnal.fi>
2026-02-10 13:38:10 +0200

    testing(mirror): Fix launch order so cleanup catches the mirror

--- testing/scripts/run-mirror.sh
+++ testing/scripts/run-mirror.sh
@@ -7,10 +7,12 @@ MIRROR_PORT="${MIRROR_PORT:-31625}"
 zip=$(readlink -f "${1?}")
 
 mkdir -p snapshot && cd snapshot
-unzip -qo "$zip"
-
 python3 -m http.server -b 127.0.0.1 "$MIRROR_PORT" -d . &
 MIRROR_PID="$!"
 echo "$MIRROR_PID" > ../mirror.pid
-
 >&2 echo "Mirror pid: $MIRROR_PID"
+
+# Unzip last as it takes a bit of time, but it'll be ready by the 
+# time  the first test pulls on the mirror.
+# This allows recording the PID earlier and allows for cleanup on failures
+unzip -qo "$zip"
