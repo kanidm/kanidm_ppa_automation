497bf6765de5dffbc743335c4e1f8cf1b382d898
Jinna Kiisuo <jinnak@nocturnal.fi>
2026-05-01 10:35:41 +0300

    chore(lint): Shell lint the whole thing before actual changes

--- testing/scripts/run-all.sh
+++ testing/scripts/run-all.sh
@@ -12,20 +12,20 @@ TELNET_PORT="${TELNET_PORT:-4321}"
 export SERIAL_CONFIG="${SERIAL_CONFIG:-telnet:localhost:${TELNET_PORT},server,nowait}" # Useful to override for debugging obscure qemu issues
 export CATEGORY="${CATEGORY:-stable}"
 export USE_LIVE="${USE_LIVE:-false}"
-export USE_DEBDIR="${USE_DEBDIR:-false}"  # If not false, expected to be a directory
-export KANIDM_VERSION="${KANIDM_VERSION:-*}"  # * default picks latest
-export KANIDM_UPGRADE="${KANIDM_UPGRADE:-false}"  # To test upgrades: set KANIDM_VERSION to an old version and this to true
+export USE_DEBDIR="${USE_DEBDIR:-false}"         # If not false, expected to be a directory
+export KANIDM_VERSION="${KANIDM_VERSION:-*}"     # * default picks latest
+export KANIDM_UPGRADE="${KANIDM_UPGRADE:-false}" # To test upgrades: set KANIDM_VERSION to an old version and this to true
 export IDM_USER="${IDM_USER:-$USER}"
-export IDM_PORT="${IDM_PORT:-58915}"  # Only relevant if IDM_URI=local
-export SSH_PUBLICKEY="${SSH_PUBLICKEY:-none}"  # Only relevant if IDM_URI=local
-export ALLOW_UNSIGNED="${ALLOW_UNSIGNED:-true}"  # Only relevant if USE_LIVE=false
-export OSARCH="${OSARCH:-$(dpkg --print-architecture)}"  # cross-arch is in no way guaranteed to work
-export CPUARCH="${CPUARCH:-$(uname -m)}"  # But if you insist, override both
-TEST_TARGETS="${TEST_TARGETS:-}"  # Single string space separated which targets to run. Default runs all
-export PRETEND_TARGET="${PRETEND_TARGET:-false}"  # Force all TEST_TARGETS to install packages from this target
-SUCCESS_WAIT="${SUCCESS_WAIT:-true}"  # Ask for confirmation before continuing to the next test after a success
+export IDM_PORT="${IDM_PORT:-58915}"                    # Only relevant if IDM_URI=local
+export SSH_PUBLICKEY="${SSH_PUBLICKEY:-none}"           # Only relevant if IDM_URI=local
+export ALLOW_UNSIGNED="${ALLOW_UNSIGNED:-true}"         # Only relevant if USE_LIVE=false
+export OSARCH="${OSARCH:-$(dpkg --print-architecture)}" # cross-arch is in no way guaranteed to work
+export CPUARCH="${CPUARCH:-$(uname -m)}"                # But if you insist, override both
+TEST_TARGETS="${TEST_TARGETS:-}"                        # Single string space separated which targets to run. Default runs all
+export PRETEND_TARGET="${PRETEND_TARGET:-false}"        # Force all TEST_TARGETS to install packages from this target
+SUCCESS_WAIT="${SUCCESS_WAIT:-true}"                    # Ask for confirmation before continuing to the next test after a success
 TEST_ROOT="$(readlink -f "$(dirname "$0")"/..)"
-export MISE_TASK_NAME="${MISE_TASK_NAME:-custom}"  # Set by Mise when running defined test sets
+export MISE_TASK_NAME="${MISE_TASK_NAME:-custom}" # Set by Mise when running defined test sets
 
 if [[ "$IDM_URI" == "local" ]] && [[ "$SSH_PUBLICKEY" == "none" ]]; then
   >&2 echo "SSH_PUBLICKEY must be set for IDM_URI=local"
@@ -42,51 +42,49 @@ source lib/log.sh
 # shellcheck source=../lib/targets.sh
 source lib/targets.sh
 
-function cleanup(){
+function cleanup() {
   set +e
   >&2 echo "Killing background processes..."
   rm -fr snapshot/*
-  sleep 2  # Give a bit of time for any proper shutdown that may work
-  [[ -f mirror.pid ]] && kill "$(cat mirror.pid)" ; ( rm mirror.pid 2>/dev/null )
-  [[ -f qemu.pid ]]   && kill "$(cat qemu.pid)"   ; ( rm qemu.pid 2>/dev/null )
+  sleep 2 # Give a bit of time for any proper shutdown that may work
+  [[ -f mirror.pid ]] && kill "$(cat mirror.pid)"
+  (rm mirror.pid 2>/dev/null)
+  [[ -f qemu.pid ]] && kill "$(cat qemu.pid)"
+  (rm qemu.pid 2>/dev/null)
 }
 
-
-function prompt(){
+function prompt() {
   read -rp "Happy? ^C to stop full run, enter to continue to next target."
 }
 
-
-
-function run(){
-	distro="$1"
-	scripts/launch-one.sh "$CPUARCH" "images/$(basename "${!distro}")"  || exit 1
-	[[ "$SUCCESS_WAIT" == "true" ]] && prompt
-	sleep 2s  # Wait for qemu to release ports
+function run() {
+  distro="$1"
+  scripts/launch-one.sh "$CPUARCH" "images/$(basename "${!distro}")" || exit 1
+  [[ "$SUCCESS_WAIT" == "true" ]] && prompt
+  sleep 2s # Wait for qemu to release ports
 }
 
-
-function get_images(){
+function get_images() {
   targets=("$@")
   mkdir -p images
 
   # Expand targets to images
   images=()
   for target in "${targets[@]}"; do
-          images+=("${!target}")
+    images+=("${!target}")
   done
 
   # Download & prep all requested target images
   log "$GREEN" "Preloading any missing target images"
   for url in "${images[@]}"; do
-          file="$(basename "$url")"
-          if [[ ! -f "images/${file}" ]]; then
-                  wget "$url" -O "images/${file}"
-                  log "$GREEN" "Resizing $file so the dpkg operations & debug binaries will fit"
-                  # The bump is quite big as the worst case scenario has ~100MiB empty after
-                  # debug deb installs even with this size. This does not increase the on-disk size.
-                  qemu-img resize "images/${file}" +1.5G  
-          fi
+    file="$(basename "$url")"
+    if [[ ! -f "images/${file}" ]]; then
+      wget "$url" -O "images/${file}"
+      log "$GREEN" "Resizing $file so the dpkg operations & debug binaries will fit"
+      # The bump is quite big as the worst case scenario has ~100MiB empty after
+      # debug deb installs even with this size. This does not increase the on-disk size.
+      qemu-img resize "images/${file}" +1.5G
+    fi
   done
 }
 
@@ -94,7 +92,7 @@ function get_images(){
 
 # If we already have a target list, use it as-is
 if [[ -n "$TEST_TARGETS" ]]; then
-  IFS=" " read -r -a targets <<< "$TEST_TARGETS"
+  IFS=" " read -r -a targets <<<"$TEST_TARGETS"
 else
   # Base set of targets: Latest LTS releases
   targets=(trixie noble)
@@ -119,7 +117,7 @@ if [[ "$USE_LIVE" == "false" && "$USE_DEBDIR" == "false" ]]; then
     exit 1
   fi
   scripts/run-mirror.sh kanidm_ppa_snapshot.zip &
-  sleep 2s  # A bit of time for the unzip before we try to use the mirror
+  sleep 2s # A bit of time for the unzip before we try to use the mirror
 fi
 
 modestring="mirror snapshot, version: ${KANIDM_VERSION}/${CATEGORY}"
@@ -140,6 +138,6 @@ for target in "${targets[@]}"; do
 done
 log "$GREEN" "Done with all targets"
 
-set +e  # Allow cleanup to fail
+set +e # Allow cleanup to fail
 cleanup || exit 0
 exit 0
